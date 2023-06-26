3551c272cbf35ece4f27c6ca5a1c652f831571f0
AlanTuring2030 <javilai2015@gmail.com>
2023-06-25 17:30:37 -0700

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - firts words
+ *
+ * Description: print on the screen
+ *
+ * Return: sucess
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
