840724106ef90cfca64f2eaa6361316eeb196d5c
AlanTuring2030 <javilai2015@gmail.com>
2023-06-25 19:39:38 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - characters upper and lower case
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+char alfabeto, e, q;
+e = 'e';
+q = 'q';
+for (alfabeto = 'a'; alfabeto, <= 'z'; alfabeto++)
+{
+if (alfabeto, != e && alfabeto, != q)
+putchar(alfabeto);
+}
+putchar('\n');
+return (0);
+}
