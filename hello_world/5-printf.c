e48d687832c2cf5377fee2284e507b516a5604d2
AlanTuring2030 <javilai2015@gmail.com>
2023-06-25 17:37:37 -0700

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - second words
+ *
+ * Description: print please
+ *
+ * Return: sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
