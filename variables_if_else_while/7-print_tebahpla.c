91f2d3541006b11c1b15a79c1106d3ec2cbc786d
AlanTuring2030 <javilai2015@gmail.com>
2023-07-04 21:00:33 -0700

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:This program print the alphabt in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'Z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
