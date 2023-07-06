f3b79678f7b7b6858fd36cba68ab775c0b4c2462
AlanTuring2030 <javilai2015@gmail.com>
2023-07-05 23:01:29 -0700

    prints 10 times the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Description: This program prints the alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char  letter;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
