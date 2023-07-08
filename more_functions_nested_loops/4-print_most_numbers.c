245969a1cadc04ffebd0bf2875403ca88f783999
AlanTuring2030 <javilai2015@gmail.com>
2023-07-08 00:59:48 -0700

    prints the numbers from 0 to 9

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to prints specific numbers
+ *
+ * Description: Prints the numbers 0 through 9 minus the numbers 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
