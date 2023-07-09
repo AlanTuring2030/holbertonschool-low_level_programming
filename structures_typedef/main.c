9c97d4af2967f0d1c37fbe3d641d50da513b24d5
AlanTuring2030 <javilai2015@gmail.com>
2023-07-09 14:25:16 -0700

    prototypes

--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -1,6 +1,4 @@
-int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
