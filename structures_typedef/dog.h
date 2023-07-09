76201dc63ee58206595c193de96b83e31f8a47ba
AlanTuring2030 <javilai2015@gmail.com>
2023-07-09 14:36:36 -0700

    struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure of a dog
+ *
+ * @name: Pointer to the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to the owner's name
+ *
+ * Description: This structure represents a dog with its name,
+ * age and owner's name
+ */
+typedef struct dog, dog_t;
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
