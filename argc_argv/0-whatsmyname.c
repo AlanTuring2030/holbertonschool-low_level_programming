c825bf38e9f97b5ca8d7048e62a921349827eb3f
AlanTuring2030 <javilai2015@gmail.com>
2023-07-09 01:49:04 -0700

    prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Program entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of character strings containing the arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
