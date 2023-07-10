165c745f78642ce7eb28e17d9b08434fbbe6b8dd
AlanTuring2030 <javilai2015@gmail.com>
2023-07-09 19:32:44 -0700

    Function that executes a function as a parameter of an array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - Function to pointers
+ * @action: pointer to function
+ * @size: size to char
+ * @array: pointer to int
+ *
+ * Description: Function that executes a function parameter of array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if ((array != NULL) && (action != NULL))
+{
+for (i = 0; i < size; i++)
+{
+if (array[i] >= 0)
+	action(array[i]);
+}
+}
+}
